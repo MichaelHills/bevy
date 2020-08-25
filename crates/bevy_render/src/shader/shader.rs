4242eac2665af911a31b45be8641026564e6c3ab
Michael Hills <mhills@gmail.com>
2020-08-25 20:19:04 +1000

    WIP naga

--- crates/bevy_render/src/shader/shader.rs
+++ crates/bevy_render/src/shader/shader.rs
@@ -36,12 +36,12 @@ fn glsl_to_spirv(
 }
 
 #[cfg(target_os = "ios")]
-impl Into<shaderc::ShaderKind> for ShaderStage {
-    fn into(self) -> shaderc::ShaderKind {
+impl Into<naga::ShaderStage> for ShaderStage {
+    fn into(self) -> naga::ShaderStage {
         match self {
-            ShaderStage::Vertex => shaderc::ShaderKind::Vertex,
-            ShaderStage::Fragment => shaderc::ShaderKind::Fragment,
-            ShaderStage::Compute => shaderc::ShaderKind::Compute,
+            ShaderStage::Vertex => naga::ShaderStage::Vertex,
+            ShaderStage::Fragment => naga::ShaderStage::Fragment,
+            ShaderStage::Compute => naga::ShaderStage::Compute,
         }
     }
 }
@@ -52,25 +52,27 @@ fn glsl_to_spirv(
     stage: ShaderStage,
     shader_defs: Option<&[String]>,
 ) -> Vec<u32> {
-    let mut compiler = shaderc::Compiler::new().unwrap();
-    let mut options = shaderc::CompileOptions::new().unwrap();
+    let module = naga::front::glsl_new::parse_str(
+        glsl_source,
+        "main".to_string(),
+        naga::ShaderStage::Vertex,
+    );
+
+    let spv = naga::back::spv::Writer::new(&module.header, naga::back::spv::WriterFlags::NONE).write(&module);
+
     if let Some(shader_defs) = shader_defs {
         for def in shader_defs.iter() {
-            options.add_macro_definition(def, None);
+            // options.add_macro_definition(def, None);
         }
     }
+    let spv_bytes: Vec<u8> = spv
+        .iter()
+        .fold(Vec::with_capacity(spv.len() * 4), |mut v, w| {
+            v.extend_from_slice(&w.to_le_bytes());
+            v
+        });
 
-    let binary_result = compiler
-        .compile_into_spirv(
-            glsl_source,
-            stage.into(),
-            "shader.glsl",
-            "main",
-            Some(&options),
-        )
-        .unwrap();
-
-    binary_result.as_binary().to_vec()
+    bytes_to_words(&spv_bytes)
 }
 
 fn bytes_to_words(bytes: &[u8]) -> Vec<u32> {
